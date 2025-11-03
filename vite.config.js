99f696efeef12e9aca5ab3afa12c39b33822de1b
mac <imac@MacBook-Air-imac.local>
2025-11-04 01:07:54 +0300

    fix: add image imports and fix paths for Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zadanie4/',
+  root: '.',
+  publicDir: 'public',
+  base: '/zadanie4/', 
   build: {
     outDir: 'dist',
     emptyOutDir: true
